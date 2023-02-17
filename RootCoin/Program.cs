3780802c2ed25db4bfb37785f1b0067f9e3be091
bopellopa <bellak20@byu.edu>
2023-02-16 23:15:20 -0700

    vid 2

--- RootCoin/Program.cs
+++ RootCoin/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace RootCoin
 {
@@ -10,7 +11,9 @@ namespace RootCoin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Block newblock = new Block(1, DateTime.Now.ToString("yyyyMMddHHmmssffff"), "amount: 50", "");
+            string blockJSON = JsonConvert.SerializeObject(newblock, Formatting.Indented);
+            Console.WriteLine(blockJSON);
         }
     }
 
@@ -18,5 +21,32 @@ namespace RootCoin
     {
 
     }
+
+    class Block
+    {
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string Timestamp { get; set; }
+        public string Data { get; set; }
+        public string Hash { get; set; }
+
+        public Block(int index, string timestamp, string data, string previousHash = "")
+        {
+            this.Index = index;
+            this.Timestamp = timestamp;
+            this.Data = data;
+            this.PreviousHash = previousHash;
+
+        }
+
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.PreviousHash + this.Timestamp + this.Data;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashBytes = SHA256.Create().ComputeHash(blockBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+    }
 }
+
 
